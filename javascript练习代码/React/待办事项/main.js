f0f67ae98b87c6759ad4a8977e043e5f2497039e
René Tan <165171058+hackematrix@users.noreply.github.com>
2025-04-12 21:13:34 +0800

    Create main.js

---
+++ javascript练习代码/React/待办事项/main.js
@@ -0,0 +1,102 @@
+/*
+  1. Implement adding and deleting todo items.
+  2. Implement completing todo items (completed items should be moved to the bottom).
+*/
+
+function MyApp() {
+  const [currentTodoItem,setCurrentTodoTtem]=React.useState("");
+  const [todoList,setTodoList]=React.useState([
+    {
+      id:1,
+      item:"Learn React",
+      isCompleted:false,
+    },
+    {
+      id:2,
+      item:"Learn Svelte",
+      isCompleted:true,
+    },
+  ]);
+
+  const sortTodo=(o1,o2)=>{
+    if(o1.isCompleted!==o2.isCompleted){
+      return o1.isCompleted?1:-1;
+    }
+    return o1.id-o2.id;
+  };
+  
+  
+  function handleAddTodo(){
+    if(!currentTodoItem.length){
+      alert("Please enter a todo Item");
+      return;
+    }
+
+    const addedList=[
+      ...todoList,
+      {
+        id:Math.random()+Date.now(),
+        item:currentTodoItem,
+        isCompleted:false,
+      }
+    ];
+
+    addedList.sort(sortTodo);
+
+    setTodoList(addedList);
+
+    setCurrentTodoTtem("");
+  }
+  
+  function handleDeleteTodo(id){
+    const deletedTodoList=todoList.filter((todo)=>todo!==id);
+    deletedTodoList.sort(sortTodo);
+    setTodoList(deletedTodoList);
+  }
+
+  function toggleTodo(id){
+    const toggleTodoList=todoList.map((todo)=>{
+        if(todo.id===id){
+          return{
+            ...todo,
+            isCompleted:!todo.isCompleted,
+          };
+        }
+
+        return todo;
+      });
+
+      toggleTodoList.sort(sortTodo);
+
+      setTodoList(toggleTodoList);
+  }
+
+  return (
+    <main>
+      <h1>React Todo List</h1>
+      <input type="text" placeholder="Add item into as todo"
+       value={currentTodoItem} 
+       onChange={(event)=>
+        setCurrentTodoTtem(event.target.value)}
+       />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+       {todoList.map((todo)=>
+              <li className={todo.isCompleted?"deleted":""} key={todo.id}>
+              <input type="checkbox"
+              defaultChecked={todo.isCompleted}
+              onClick={()=>toggleTodo(todo.id)} />
+              {todo.item}
+              <button onClick={()=>handleDeleteTodo(todo.id)}>delete</button>
+            </li>
+       )}
+      </ul>    
+       
+    </main>
+  );
+}
+
+const appEl = document.querySelector("#app");
+const root = ReactDOM.createRoot(appEl);
+
+root.render(<MyApp />);
