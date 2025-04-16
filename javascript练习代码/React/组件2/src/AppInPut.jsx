435eab8b7fa57ba99f9c2501b55f6d4b123344ff
René Tan <165171058+hackematrix@users.noreply.github.com>
2025-04-16 23:39:23 +0800

    Create AppInPut.jsx

---
+++ javascript练习代码/React/组件2/src/AppInPut.jsx
@@ -0,0 +1,15 @@
+function AppInput({className,type,value,setState}){
+    return(
+        <input
+        className={className}
+        type={type}
+        value={value}
+        onChange={(event)=>setState(event.target.value)}
+        />
+
+    );
+
+    
+}
+
+export default AppInput;
