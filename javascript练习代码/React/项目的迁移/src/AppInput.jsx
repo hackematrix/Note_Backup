7d5fcaba5b6c59a902b0cff1e1c4e27260019004
René Tan <165171058+hackematrix@users.noreply.github.com>
2025-04-17 10:50:24 +0800

    Create AppInput.jsx

---
+++ javascript练习代码/React/项目的迁移/src/AppInput.jsx
@@ -0,0 +1,16 @@
+function AppInput({value,setState,children}){
+    return(
+        <label>
+        {children}
+        <input
+          type="number"
+          value={value}
+          onChange={(event) =>
+            setState(event.target.value)
+          }
+        />
+      </label>
+    );
+}
+
+export default AppInput;
