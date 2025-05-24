84b4b8568ad249dc31c46535af353b03d7c360b3
René Polchniski <165171058+hackematrix@users.noreply.github.com>
2025-05-25 00:00:15 +0800

    Create main.js

---
+++ jsPsych学习/javascript(下）/switch判断/main.js
@@ -0,0 +1,15 @@
+let rating=Number(prompt("输入满意程度"));
+
+switch(rating){
+  case 1:
+    console.log("非常不满意");break;
+  case 2:
+    console.log("不满意");break;
+  case 3:
+    console.log("中立");break;
+  case 4:
+    console.log("满意");break;
+  case 5:
+    console.log("非常满意");break;      
+
+}
